3b80c528d99f7d9e419e91bcf433cdaf04bd6f80
felipemassun935 <felipemassun14@gmail.com>
2026-04-30 10:48:50 -0300

    automation

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
-
+//FUNCTION
 export async function nextTicketId() {
   const { value } = await prisma.counter.update({
     where: { key: 'ticket' },
